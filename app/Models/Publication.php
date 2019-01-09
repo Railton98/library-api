f576e2deecb40cda3cc5423a594c5b19abb696b3
Railton98 <railtonleal98@gmail.com>
2019-01-09 17:13:19 -0300

    add fields short_description and full_description for fillable property

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -8,7 +8,11 @@ use App\Models\Exemplary;
 class Publication extends Model
 {
     protected $fillable = [
-        'title', 'author', 'image', 'publication_year', 'type',
+        'title', 'author', 'image', 'publication_year', 'type', 'short_description', 'full_description',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
     ];
 
     public function exemplaries()
